141b5d554a60fd96e0d9543b346c46a39c3e03c3
hari070801 <67643539+hari070801@users.noreply.github.com>
2022-07-27 17:14:58 +0530

    Update Prog4.java

--- Prog4.java
+++ Prog4.java
@@ -1,75 +1,53 @@
-package com.example.changebackground;
-import androidx.appcompat.app.AppCompatActivity; 
+package com.example.prog4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.app.WallpaperManager;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.AnimationDrawable; 
-import android.graphics.drawable.BitmapDrawable; 
-import android.graphics.drawable.Drawable;
-import android.os.Bundle; 
-import android.view.View; 
-import android.widget.Button; 
-import android.widget.Toast; 
-import java.io.IOException; 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity {
-Button changewallpaper; 
-Timer mytimer;
-  Drawable drawable; 
-  WallpaperManager wpm; 
-  int prev=1;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btn;
+    boolean running;    //running or not running
 
-@Override
-protected void onCreate(Bundle savedInstanceState) { 
-  super.onCreate(savedInstanceState); 
-  setContentView(R.layout.activity_main);
-mytimer = new Timer();
-wpm = WallpaperManager.getInstance(this); 
-  changewallpaper = findViewById(R.id.button);
-changewallpaper.setOnClickListener(new View.OnClickListener() { 
-  @Override
-public void onClick(View view) { 
-  setWallpaper();
-}
-});
-}
+    //create array to store images
+    int[] img = new int[]{R.drawable.img1,R.drawable.img2,R.drawable.img3};
 
-private void setWallpaper()
-{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-mytimer.schedule(new TimerTask() { 
-  @Override
-public void run() { 
-  if(prev==1) {
-drawable = getResources().getDrawable(R.drawable.one); 
-    prev = 2;
-}
-else if(prev==2) {
-drawable = getResources().getDrawable(R.drawable.two); 
-  prev=3;
-}
-else if(prev==3) {
-drawable = getResources().getDrawable(R.drawable.three); 
-  prev=4;
-}
-else if(prev==4) {
-drawable = getResources().getDrawable(R.drawable.four); 
-  prev=5;
-}
-else if(prev==5) {
-drawable = getResources().getDrawable(R.drawable.five); 
-  prev=1;
-}
-Bitmap wallpaper = ((BitmapDrawable)drawable).getBitmap(); 
-  try {
-wpm.setBitmap(wallpaper);
-}
-catch (IOException e) { 
-  e.printStackTrace();
-}
-}
-},0,30000);
-}
+        btn = (Button) findViewById(R.id.wallbtn);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(!running){
+            new Timer().schedule(new myTimer(),0,3000);
+            running = true;
+        }
+    }
+
+    private class myTimer extends TimerTask {
+
+        @Override
+        public void run() {
+            try{
+                WallpaperManager wallpaperManager = WallpaperManager.getInstance(getBaseContext());
+                Random random = new Random();
+                wallpaperManager.setBitmap(BitmapFactory.decodeResource(getResources(),img[random.nextInt(3)]));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
