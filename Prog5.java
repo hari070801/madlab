87e423ef31deef074ce2b06777dabb2cbdbd8e22
hari070801 <67643539+hari070801@users.noreply.github.com>
2022-07-27 17:16:28 +0530

    Update Prog5.java

--- Prog5.java
+++ Prog5.java
@@ -1,62 +1,77 @@
-package com.example.mycounter;
-import androidx.appcompat.app.AppCompatActivity; 
-import android.os.Bundle;
-import android.os.Handler; 
-import android.os.Message; 
-import android.view.View; 
-import android.widget.Button; 
-import android.widget.TextView; 
-import org.w3c.dom.Text;
+package com.example.counterapplication;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{ 
-TextView lblCounter;
-Button btnStart,btnStop; 
-int counter=0;
-boolean running=false; 
-  
-@Override
-protected void onCreate(Bundle savedInstanceState) { 
-super.onCreate(savedInstanceState); 
-  setContentView(R.layout.activity_main); 
-  lblCounter=(TextView)findViewById(R.id.lbl_text); 
-  btnStart=(Button)findViewById(R.id.btn_start); 
-  btnStop=(Button)findViewById(R.id.btn_stop); 
-  btnStop.setOnClickListener(this); 
-  btnStart.setOnClickListener(this);
-}
-public void onClick(View v)
-{
-if(v.equals(btnStart))
-{
-counter=0; 
-running=true;
-new MyCounter().start();
-}
-else if(v.equals(btnStop))
-{
-running=false;
-}
-}
-Handler handler=new Handler()
-{
-public void handleMessage(Message m)
-{
-lblCounter.setText(String.valueOf(m.what));
-}
-};
-class MyCounter extends Thread
-{
-public void run()
-{
-while(running)
-{
-counter++;
-handler.sendEmptyMessage(counter); 
-try {
-Thread.sleep(1000);
-}
-catch(Exception e) { }
-}
-}
-}
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonstart,buttonstop;
+    TextView countervalue;
+    public int counter=0;
+    public boolean running = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonstart = (Button) findViewById(R.id.btnstart);
+        buttonstart.setOnClickListener(this);
+        buttonstop = (Button) findViewById(R.id.btnstop);
+        buttonstop.setOnClickListener(this);
+        countervalue = (TextView) findViewById(R.id.txtvalue);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.equals(buttonstart)){
+            counterStart();
+        }else if(v.equals(buttonstop)){
+            counterStop();
+        }
+    }
+
+    private void counterStop() {
+        this.running=false;
+        buttonstart.setEnabled(true);
+        buttonstop.setEnabled(false);
+    }
+
+    private void counterStart() {
+        counter=0;
+        running = true;
+        new MyCounter().start();
+        buttonstart.setEnabled(false);
+        buttonstop.setEnabled(true);
+    }
+
+    Handler handler = new Handler(Looper.getMainLooper())
+    {
+        public void handleMessage(Message mes){
+            countervalue.setText(String.valueOf(mes.what));
+        }
+
+    };
+
+    class MyCounter extends Thread{
+        public void run(){
+            while(running){
+                counter++;
+
+                handler.sendEmptyMessage(counter);
+                try{
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
